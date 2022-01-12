8011cfe15dd1172d43b9e0157b5b2665c20ffe9d
Jonathan Shimwell <mail@jshimwell.com>
2022-01-12 14:36:58 +0000

    added example that uses Plasma and StorageAndDeliverySystem

--- examples/flf_example.py
+++ examples/flf_example.py
@@ -4,28 +4,28 @@ import system_code as tsc
 
 plasma = tsc.Plasma(
     name="Plasma",
-    outputs ={"Storage": 1},
+    outputs={"Storage": 1},
     volume=1,
     plasma_burning_rate=1,
-    initial_concentration=0
+    initial_concentration=0,
 )
 
 
 breeder = tsc.Box(
     name="Breeder",
-    outputs ={"Storage": 1},
+    outputs={"Storage": 1},
     volume=1,
     generation_term=1.05,
-    initial_concentration=0
+    initial_concentration=0,
 )
 
 
 storage = tsc.StorageAndDeliverySystem(
     name="Storage",
-    output_name ="Plasma",
+    output_name="Plasma",
     volume=1,
     fueling_rate=1,
-    initial_concentration=5
+    initial_concentration=5,
 )
 
 
