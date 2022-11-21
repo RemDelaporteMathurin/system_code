94958ef0d02294b3163a89a8a460ea9f1b331c2a
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2022-11-21 15:27:48 -0500

    added link

--- system_code/__init__.py
+++ system_code/__init__.py
@@ -1,5 +1,6 @@
 LAMBDA = 1.678e-9  # in s-1
 
+from .link import Link
 from .system import System
 from .box import Box
 from .fueling_system import StorageAndDeliverySystem
---
+++ system_code/link.py
@@ -0,0 +1,5 @@
+class Link:
+    def __init__(self, origin, target, mass_flow) -> None:
+        self.origin = origin
+        self.target = target
+        self.mass_flow = mass_flow
