66e27a6a304531829dad198fe16d3974afcc0139
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2022-11-21 16:40:13 -0500

    fixed exampels

--- examples/balanced_scenario.py
+++ examples/balanced_scenario.py
@@ -1,10 +1,13 @@
 import system_code as tsc
 
 storage = tsc.Box("Storage", {"Plasma": 0.45}, volume=100, initial_concentration=1/100)
-plasma = tsc.Box("Plasma", {}, volume=1, generation_term=-1, initial_concentration=1)
-breeder = tsc.Box("Breeder", {"Storage": 1}, volume=1, generation_term=1.05)
-out = tsc.Box("Out", outputs={}, volume=1, initial_concentration=0)
-my_system = tsc.System([storage, plasma, breeder, out])
+plasma = tsc.Box("Plasma", volume=1, generation_term=-1, initial_concentration=1)
+breeder = tsc.Box("Breeder", generation_term=1.05)
+
+breeder.add_output(storage, 1)
+storage.add_output(plasma, 0.45)
+
+my_system = tsc.System([storage, plasma, breeder])
 # my_system.run(20)
 while my_system.current_time < 20:
     my_system.advance()
