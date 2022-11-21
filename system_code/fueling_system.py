9843f024d624138025f7bcb1cd80a34bb1a0a2a7
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2022-11-21 16:46:10 -0500

    fixed flf example

--- system_code/fueling_system.py
+++ system_code/fueling_system.py
@@ -2,14 +2,14 @@ from system_code import Box
 
 
 class StorageAndDeliverySystem(Box):
-    def __init__(self, name, output_name, volume, fueling_rate, initial_concentration):
+    def __init__(self, name, output, volume, fueling_rate, initial_concentration):
         self.fueling_rate = fueling_rate
         self.flowrate = fueling_rate/initial_concentration
-        self.output_name = output_name
-        outputs = {output_name: self.flowrate}
+        self.output = output
+        outputs = {output: self.flowrate}
         super().__init__(name, outputs, volume, initial_concentration=initial_concentration, generation_term=0)
 
     def update(self):
         """Updates the flowing rate to ensure a fixed fueling rate
         """
-        self.outputs[self.output_name] = self.fueling_rate/self.concentration
+        self.outputs[self.output] = self.fueling_rate/self.concentration
